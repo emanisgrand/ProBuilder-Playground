06a7a88bf8faba7784b8971b02f1a60950478b83
eman <tenhitcombo@gmail.com>
2019-11-09 16:56:29 -0500

    created the bullet pool and populated it with current bullet behavior

--- Assets/Scripts/Pooling/PoolManager.cs
+++ Assets/Scripts/Pooling/PoolManager.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+public class PoolManager : MonoBehaviour {
+    
+    private static PoolManager _instance;
+    public static PoolManager Instance {
+        get {
+            if (_instance == null)
+                Debug.Log("The PoolManager is null");
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    
+    [SerializeField] private List<GameObject> _bulletPool;
+
+    private void Awake() {
+        _instance = null;
+    }
+
+    private void Start() {
+        _bulletPool = GenerateBullets(10);
+    }
+
+    List<GameObject> GenerateBullets(int amountOfBullets) {
+        for (int i = 0; i < amountOfBullets; i++) {
+            GameObject bullet = Instantiate(_bulletPrefab);
+            _bulletPool.Add(bullet);
+        }
+
+        return _bulletPool;
     }
 }

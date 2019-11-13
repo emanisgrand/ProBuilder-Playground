6c5aaedd5ad5a10a5ec6452892f13b08f7172432
Eman Martinez <emanisgrand@users.noreply.github.com>
2019-11-13 17:17:06 -0500

    Update Bullet.cs
    Adds logic to support pooling the game object by cycling through SetActive On Enable.
    
    In other words, when the bullet is enabled, the OnEnable function Invokes the `Hide` function for 1 second.
    Hide simply SetsActive to false which is then switched to true when you press space bar. That logic is in the PoolManager script.

--- Assets/Scripts/Pooling/Bullet.cs
+++ Assets/Scripts/Pooling/Bullet.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     // Start is called before the first frame update
-    void Start() {
-        
+    private void OnEnable() {
+        Invoke("Hide", 1);
+    }
+
+    void Hide() {
+        this.gameObject.SetActive(false);
     }
 }
